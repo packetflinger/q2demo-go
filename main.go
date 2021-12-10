b762f465a1c8dcb51076a0b0c89361f033ec60ff
Joe Reid <joe@joereid.com>
2021-12-09 20:41:18 -0500

    lame arg checking

--- main.go
+++ main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"os"
 
 	"github.com/packetflinger/q2demo/dm2"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s <demofile.dm2>\n", os.Args[0])
+		return
+	}
+
 	position := 0
-	demofile := dm2.OpenDemo("test.dm2")
+	demofile := dm2.OpenDemo(os.Args[1])
 
 	for {
 		lump, size := dm2.NextLump(demofile, int64(position))
 		if size == 0 {
-			break;
+			break
 		}
 
 		fmt.Printf("%s\n", hex.Dump(lump))
